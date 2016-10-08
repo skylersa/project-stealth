40d67b9134ff80662eea5ab521f0ce0690165ac5
Skyler Sauer <sjwsauer@gmail.com>
2016-10-08 13:40:16 -0700

    can crouch

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -12,5 +12,18 @@ public class CharacterMovement : MonoBehaviour {
 		transform.position += Input.GetAxis("Vertical") * speed * Time.deltaTime * transform.forward;
 		transform.position += Input.GetAxis("Horizontal") * speed * Time.deltaTime * transform.right;
 		transform.position += Input.GetAxis("Jump") * speed * Time.deltaTime * transform.up;
+
+		// deafult local scale = (1,1,1)
+		// crouch local scale = (1,.5,1)
+
+		// Input.GetAxis ("Crouch") == 0   <--- default
+		// Input.GetAxis ("Crouch") == -1  <--- crouching
+
+		// Input.GetAxis ("Crouch")       -->    y local scale
+		//              0                 -->          1.0
+		//             -1                 -->          0.5
+
+		float yLocalScale = Input.GetAxis ("Crouch") *.5f + 1f;
+		transform.localScale = new Vector3 (1f, yLocalScale, 1f);
 	}
 }
