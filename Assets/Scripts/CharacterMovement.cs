39cd2484a522c00004b739e571f9f0c9a6096c88
Skyler Sauer <sjwsauer@gmail.com>
2016-10-08 13:09:18 -0700

    initial commit

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+	public float speed = 0.00001f; 
+
+	Vector3 movement;  
+
+	void Update () {
+//		if (Input.GetKey (KeyCode.W))
+//			GetComponent<Rigidbody>().MovePosition (
+		transform.position += Input.GetAxis("Vertical") * speed * Time.deltaTime * transform.forward;
+		transform.position += Input.GetAxis("Horizontal") * speed * Time.deltaTime * transform.right;
+		transform.position += Input.GetAxis("Jump") * speed * Time.deltaTime * transform.up;
+	}
+}
